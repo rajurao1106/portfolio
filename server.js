52db1425ba02eb695e0e3b169cb45db20828b667
rajurao <rajurao1107@gmail.com>
2025-02-17 16:28:10 +0530

    commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = process.env.PORT || 2006;
+const PORT = process.env.PORT || 2007;
 
 // Middleware
 app.use(cors());
